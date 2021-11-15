6b77c4d6f312950e5e4ff398e3b972caf4b234c8
Alberto García <>
2021-11-15 18:49:01 +0100

    no message

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var id = '1pXcfkMKdXFD3XvmuBgkgVu7UXoxYN3ZDKOoOchIjKO0';
-var gid = '1';
+var gid = '0';
 var url = 'https://docs.google.com/spreadsheets/d/'+id+'/gviz/tq?tqx=out:json&tq&gid='+gid;
 fetch(url)
   .then(response => response.text())

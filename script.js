58ac83d78dcb5c801341661a97e0866dc17c0c35
Alberto García <>
2021-11-15 18:05:40 +0100

    no message

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function myItems(jsonString){
   var json = JSON.parse(jsonString);
   var table = '<div>'
   // json.table.cols.forEach(colonne => table += '<p class="uno">' + colonne.label + '</p>')
-  table += '</p>'
+  // table += '</p>'
   json.table.rows.forEach(ligne => {
     // table += '<p class="dos">'
     ligne.c.forEach(cellule => {

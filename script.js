aec09afa95945f89fd251e7e7edcbf20950e6a79
Alberto García <>
2021-11-15 17:15:18 +0100

    qwe

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ var gid = '0';
 var url = 'https://docs.google.com/spreadsheets/d/'+id+'/gviz/tq?tqx=out:json&tq&gid='+gid;
 fetch(url)
   .then(response => response.text())
-  .then(data => document.getElementById("json").innerHTML=myItems(data.substring(47).slice(0, -2))  
+  .then(data => document.getElementById("ventas").innerHTML=myItems(data.substring(47).slice(0, -2))  
   );
 function myItems(jsonString){
   var json = JSON.parse(jsonString);
-  var table = '<p>'
+  var table = '<p class="uno">'
   json.table.cols.forEach(colonne => table += '<p>' + colonne.label + '</p>')
   table += '</p>'
   json.table.rows.forEach(ligne => {
